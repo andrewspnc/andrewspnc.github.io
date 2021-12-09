87733638720d8bfb4a4b6d8c60cc6a94514104ac
andrewspnc <andrewspnc@hotmail.com>
2021-12-09 15:45:46 +0000

    finished v1 inc contact form and JS

--- js/script.js
+++ js/script.js
@@ -780,15 +780,51 @@ const blackCard = [`${randomWhiteCard()} - kid tested mother-approved.`,
 `Why do I hurt all over? ${randomWhiteCard()}`, 
 ];
 
-const randomBlackCard = () => 
-{
+function randomBlackCard() {
   return blackCard[Math.floor(Math.random() * blackCard.length)];
+};
+
+let randomQuote = () => {
+document.getElementById("click").innerHTML = randomBlackCard();
+};
+
+function project1() {
+  document.getElementById("project1").innerHTML = "Codecademy Project using HTML & CSS";
+  document.getElementById("project1div").style.opacity = 0.5;  
+  };
+
+  function project1Reset() {
+    document.getElementById("project1").innerHTML = "";
+    document.getElementById("project1div").style.opacity = "";  
+    };
+ 
+  function project2() {
+    document.getElementById("project2div").style.opacity = 0.5;  
+      };
+    
+  function project2Reset() {
+    document.getElementById("project2div").style.opacity = "";  
+        };
+
+/*model
+*/
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
 }
 
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-
-
-/*
-let randomQuote = randomBlackCard;
-document.getElementById("click").innerHTML = randomQuote;
-
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
